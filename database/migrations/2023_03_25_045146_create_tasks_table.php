76677325372a0f3521dad9c058cf8ca7fbf9360d
TahuTech <18053036@student.uwp.ac.id>
2023-03-25 11:55:35 +0700

    task database

--- database/migrations/2023_03_25_045146_create_tasks_table.php
+++ database/migrations/2023_03_25_045146_create_tasks_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('date');
+            $table->string('time');
+            $table->text('detail');
             $table->timestamps();
         });
     }
